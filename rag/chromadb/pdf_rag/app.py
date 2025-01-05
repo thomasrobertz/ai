3badf1ce9463f4d80fe42e1b2687c133a45bca5c
Thomas Robertz <thomasrobertz@users.noreply.github.com>
2025-01-05 17:38:09 +0100

    PDF RAG QnA

---
+++ rag/chromadb/pdf_rag/app.py
@@ -0,0 +1,193 @@
+import os
+import tempfile
+
+import chromadb
+import ollama
+import streamlit as st
+from chromadb.utils.embedding_functions.ollama_embedding_function import (
+    OllamaEmbeddingFunction,
+)
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import CrossEncoder
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+system_prompt = """
+You are an AI assistant tasked with providing detailed answers based solely on the given context. Your goal is to analyze the information provided and formulate a comprehensive, well-structured response to the question.
+
+context will be passed as "Context:"
+user question will be passed as "Question:"
+
+To answer the question:
+1. Thoroughly analyze the context, identifying key information relevant to the question.
+2. Organize your thoughts and plan your response to ensure a logical flow of information.
+3. Formulate a detailed answer that directly addresses the question, using only the information provided in the context.
+4. Ensure your answer is comprehensive, covering all relevant aspects found in the context.
+5. If the context doesn't contain sufficient information to fully answer the question, state this clearly in your response.
+
+Format your response as follows:
+1. Use clear, concise language.
+2. Organize your answer into paragraphs for readability.
+3. Use bullet points or numbered lists where appropriate to break down complex information.
+4. If relevant, include any headings or subheadings to structure your response.
+5. Ensure proper grammar, punctuation, and spelling throughout your answer.
+
+Important: Base your entire response solely on the information provided in the context. Do not include any external knowledge or assumptions not present in the given text.
+"""
+
+def process_document(uploaded_file: UploadedFile) -> list[Document]:
+    """Processes an uploaded PDF file by converting it to text chunks."""
+    # Store uploaded file as a temp file
+    temp_file = tempfile.NamedTemporaryFile("wb", suffix=".pdf", delete=False)
+    try:
+        temp_file.write(uploaded_file.read())
+        temp_file.close()  # Ensure the file is properly closed
+
+        loader = PyMuPDFLoader(temp_file.name)
+        docs = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=400,
+            chunk_overlap=100,
+            separators=["\n\n", "\n", ".", "?", "!", " ", ""],
+        )
+        return text_splitter.split_documents(docs)
+    finally:
+        # Always delete the file after processing, ensuring it is no longer in use
+        if os.path.exists(temp_file.name):
+            os.unlink(temp_file.name)
+
+
+
+def get_vector_collection() -> chromadb.Collection:
+    ollama_ef = OllamaEmbeddingFunction(
+        url="http://localhost:11434/api/embeddings",
+        model_name="nomic-embed-text:latest",
+    )
+
+    chroma_client = chromadb.PersistentClient(path="./demo-rag-chroma")
+    return chroma_client.get_or_create_collection(
+        name="rag_app",
+        embedding_function=ollama_ef,
+        metadata={"hnsw:space": "cosine"},
+    )
+
+
+def add_to_vector_collection(all_splits: list[Document], file_name: str):
+    collection = get_vector_collection()
+    documents, metadatas, ids = [], [], []
+
+    for idx, split in enumerate(all_splits):
+        documents.append(split.page_content)
+        metadatas.append(split.metadata)
+        ids.append(f"{file_name}_{idx}")
+
+    collection.upsert(
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
+    )
+    st.success("Data added to the vector store!")
+
+
+def query_collection(prompt: str, n_results: int = 10):
+
+    collection = get_vector_collection()
+    results = collection.query(query_texts=[prompt], n_results=n_results)
+    return results
+
+
+def call_llm(context: str, prompt: str):
+
+    response = ollama.chat(
+        model="llama3.2:latest",
+        stream=True,
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt,
+            },
+            {
+                "role": "user",
+                "content": f"Context: {context}, Question: {prompt}",
+            },
+        ],
+    )
+    for chunk in response:
+        if chunk["done"] is False:
+            yield chunk["message"]["content"]
+        else:
+            break
+
+
+def re_rank_cross_encoders(documents: list[str]) -> tuple[str, list[int]]:
+
+    relevant_text = ""
+    relevant_text_ids = []
+
+    encoder_model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+    ranks = encoder_model.rank(prompt, documents, top_k=3)
+    for rank in ranks:
+        relevant_text += documents[rank["corpus_id"]]
+        relevant_text_ids.append(rank["corpus_id"])
+
+    return relevant_text, relevant_text_ids
+
+
+if __name__ == "__main__":
+    # Document Upload Area
+    with st.sidebar:
+        st.set_page_config(page_title="PDF RAG QA")
+        uploaded_file = st.file_uploader(
+            "**Upload PDF files for QnA**", type=["pdf"], accept_multiple_files=False
+        )
+
+        process = st.button(
+            "Process",
+        )
+        if uploaded_file and process:
+            normalize_uploaded_file_name = uploaded_file.name.translate(
+                str.maketrans({"-": "_", ".": "_", " ": "_"})
+            )
+            all_splits = process_document(uploaded_file)
+            add_to_vector_collection(all_splits, normalize_uploaded_file_name)
+
+    # Question and Answer Area
+    st.header("PDF RAG QA")
+    st.caption("⚠️ Note: Responses may take a few moments as all inference is running locally.")
+    st.text("AWS CCP Exam guide has been preloaded.")
+    
+    # Example questions that can be clicked
+    st.write("Example questions for AWS CCP:")
+    example_questions = [
+        "The exam validates which of the candidate’s abilities?",
+        "What is covered in Domain 2?",
+        "Tell me about the exam response types.",
+        "Is the AWS Well-Architected Framework part of the exam?"
+    ]
+    
+    for question in example_questions:
+        if st.button(f" {question}", key=f"btn_{hash(question)}"):
+            st.session_state.example_question = question
+            
+    prompt = st.text_area(
+        "**Ask a question related to your document:**",
+        value=st.session_state.get('example_question', ''),
+        key="question_input"
+    )
+    ask = st.button("Ask")
+
+    if ask and prompt:
+        results = query_collection(prompt)
+        context = results.get("documents")[0]
+        relevant_text, relevant_text_ids = re_rank_cross_encoders(context)
+        response = call_llm(context=relevant_text, prompt=prompt)
+        st.write_stream(response)
+
+        with st.expander("See retrieved documents"):
+            st.write(results)
+
+        with st.expander("See reranked document ids"):
+            st.write(relevant_text_ids)
+            st.write(relevant_text)
